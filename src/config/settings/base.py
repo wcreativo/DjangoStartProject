da85c7498a17500b4d92a0e6cfc1a327c2bc3c22
wcreativo <w.kreativo@gmail.com>
2024-02-04 13:00:37 -0500

    Fix settings celery port

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -87,5 +87,5 @@ CACHES = {
     }
 }
 
-CELERY_BROKER_URL = "redis://redis:6739/0"
-CELERY_RESULT_BACKEND = "redis://redis:6739/0"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
